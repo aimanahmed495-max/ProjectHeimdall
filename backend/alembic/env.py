7bd4f57c4506a10488c1a27e12772d80c13908a3
Arham Sadid Hossain <axhossain8@shockers.wichita.edu>
2026-08-27 00:36:17 -0500

    fix: escape percent signs in Alembic database URL

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -10,7 +10,10 @@ from app.database import Base, DATABASE_URL
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
+config.set_main_option(
+    "sqlalchemy.url",
+    DATABASE_URL.replace("%", "%%"),
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
